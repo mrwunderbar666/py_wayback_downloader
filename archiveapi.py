1ab0b4682693aa1dda9a494f000598567cb9cdf5
Paul Balluff <balluffp88@u.univie.ac.at>
2020-10-05 15:57:09 +0200

    user argument parsing

--- archiveapi.py
+++ archiveapi.py
@@ -5,6 +5,9 @@ import pandas as pd
 from urllib.parse import urlparse
 import concurrent.futures
 
+import argparse
+
+
 def download_archive(input_list):
     _, file_timestamp, file_url, file_type, _, _, file_length = input_list
     print(file_timestamp, file_url, file_length)
@@ -23,8 +26,10 @@ def download_archive(input_list):
         print('File already exists! Skipping...')
         return None
 
+    # construct download url
     download_url = f'https://web.archive.org/web/{file_timestamp}/{file_url}'
 
+    # download file
     r = requests.get(download_url)
 
     print('Status code: ')
@@ -34,56 +39,71 @@ def download_archive(input_list):
         print('No response! Skipping...')
         return None
 
+    # create directory structure
+
     os.makedirs(file_path, exist_ok=True)
 
+    # save file
     with open(os.path.join(file_path, file_name), 'wb') as f:
         f.write(r.content)
     return True
 
+if __name__ == '__main__':
 
-request_url =  "https://web.archive.org/cdx/search/xd"
-n_threads = 16
+    parser = argparse.ArgumentParser(description='Usage: archiveapi http://example.com \nDownload an entire website from the Wayback Machine.')
 
-base_url = "https://mainichi.jp/english"
-ts_from = 20140101
-ts_to = 20150101
-filter_mime_type = 'mimetype:text/html'
+    parser.add_argument('base_url', type=str, nargs=1, help='The website or path to download. e.g. "http://example.com"')
+    parser.add_argument('--from', type=int, nargs=1, help="Starting date to download. Timestamp in format YYYYMMDD, e.g. 20120808")
+    parser.add_argument('--to', type=int, nargs=1, help="End date to download. Timestamp in format YYYYMMDD, e.g. 20120808")
+    parser.add_argument('--threads', type=int, nargs=1, help="Number of concurrent downloads. Recommended 4 - 16")
+    
+    args = parser.parse_args()
+    print(args)
 
-if not base_url.endswith('/'):
-    base_url += '/'
+    request_url =  "https://web.archive.org/cdx/search/xd"
+    n_threads = 16
 
-base_url_parsed = urlparse(base_url)
+    base_url = "https://mainichi.jp/english"
+    ts_from = 20140101
+    ts_to = 20150101
+    filter_mime_type = 'mimetype:text/html'
 
+    if not base_url.endswith('/'):
+        base_url += '/'
 
-payload = {'url': f'{base_url}*',
-            # 'fl': 'timestamp,original',
-            'collapse': 'digest',
-            'gzip': "false",
-            'output': 'json',
-            'filter': ['statuscode:200', filter_mime_type],
-            'from': ts_from,
-            'to': ts_to,
-            }
+    base_url_parsed = urlparse(base_url)
 
-print('Requesting Index...')
+    # add maximum possible daterange as default
 
-r = requests.get(request_url, params=payload)
+    payload = {'url': f'{base_url}*',
+                # 'fl': 'timestamp,original',
+                'collapse': 'digest',
+                'gzip': "false",
+                'output': 'json',
+                'filter': ['statuscode:200', filter_mime_type],
+                'from': ts_from,
+                'to': ts_to,
+                }
 
-if r.status_code != 200:
-    print('Bad response!')
-    print('Status code:')
+    print('Requesting Index...')
+
+    r = requests.get(request_url, params=payload)
+
+    if r.status_code != 200:
+        print('Bad response!')
+        print('Status code:')
+        print(r.status_code)
+        print('Headers')
+        print(r.headers)
+        pass
+
+    print('Good Response')
     print(r.status_code)
-    print('Headers')
-    print(r.headers)
-    pass
 
-print('Good Response')
-print(r.status_code)
+    json_response = json.loads(r.content)
 
-json_response = json.loads(r.content)
+    download_list = json_response[1:]
+    print(f'Got {len(download_list)} to download...')
 
-download_list = json_response[1:]
-print(f'Got {len(download_list)} to download...')
-
-with concurrent.futures.ThreadPoolExecutor(max_workers=n_threads) as executor:
-    executor.map(download_archive, download_list)
+    # with concurrent.futures.ThreadPoolExecutor(max_workers=n_threads) as executor:
+    #     executor.map(download_archive, download_list)
